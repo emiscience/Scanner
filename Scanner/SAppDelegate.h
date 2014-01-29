019d43f483752d73164a706eae9d315028361158
Levi Dhuyvetter <levidhuyvetter@Levi-Dhuyvetters-MacBook.local>
2014-01-29 18:54:44 +0000

    First commit, scanning is working already.

--- Scanner/SAppDelegate.h
+++ Scanner/SAppDelegate.h
@@ -1,15 +1,19 @@
-//
-//  SAppDelegate.h
-//  Scanner
-//
-//  Created by Levi Dhuyvetter on 27/01/2014.
-//  Copyright (c) 2014 Levi Dhuyvetter. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
+#import <Quartz/Quartz.h>
+#import <ImageCaptureCore/ImageCaptureCore.h>
 
-@interface SAppDelegate : NSObject <NSApplicationDelegate>
+@interface SAppDelegate : NSObject <NSApplicationDelegate, ICDeviceBrowserDelegate, ICScannerDeviceDelegate> {
+    ICDeviceBrowser *deviceBrowser;
+    NSMutableArray *scanners;
+    IBOutlet NSArrayController *scannersController;
+    IBOutlet PDFView *pdfView;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) NSMutableArray *_scanners;
+
+- (IBAction)scan:(id)sender;
+- (ICScannerDevice *)selectedScanner;
+- (BOOL)scannerAvailable;
 
 @end
--- Scanner/SAppDelegate.m
+++ Scanner/SAppDelegate.m
@@ -1,25 +1,131 @@
-//
-//  SAppDelegate.m
-//  Scanner
-//
-//  Created by Levi Dhuyvetter on 27/01/2014.
-//  Copyright (c) 2014 Levi Dhuyvetter. All rights reserved.
-//
-
 #import "SAppDelegate.h"
 
 @implementation SAppDelegate
 
+//MARK: - Synthesize Properties
 @synthesize window = _window;
+@synthesize _scanners = scanners;
 
-- (void)dealloc
-{
-    [super dealloc];
+//MARK: - Application Delegate Methods	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    //Initializing and setting up the scanners array and arraycontroller
+    scanners = [[NSMutableArray alloc] initWithCapacity:0];
+    [scannersController setSelectsInsertedObjects:NO];
+    
+    //Initializing and setting up the device browser
+    deviceBrowser = [[ICDeviceBrowser alloc] init];
+    [deviceBrowser setDelegate:self];
+    [deviceBrowser setBrowsedDeviceTypeMask:ICDeviceTypeMaskScanner|
+                                            ICDeviceLocationTypeMaskLocal|
+                                            ICDeviceLocationTypeMaskRemote];
+    [deviceBrowser start];
 }
-	
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+
+//MARK: - Device Browser Delegate Methods
+- (void)deviceBrowser:(ICDeviceBrowser *)browser didAddDevice:(ICDevice *)device moreComing:(BOOL)moreComing {
+    //If the device is a scanner
+    if (([device type] & ICDeviceTypeMaskScanner) == ICDeviceTypeScanner) {
+        //Adding the device to the scanners array
+        [self willChangeValueForKey:@"_scanners"];
+        [scanners addObject:device];
+        [self didChangeValueForKey:@"_scanners"];
+        
+        //Setting up the device
+        [device setDelegate:self];
+        [device requestOpenSession];
+        [(ICScannerDevice *)device requestSelectFunctionalUnit:[[[(ICScannerDevice *)device availableFunctionalUnitTypes] objectAtIndex:0] intValue]];
+        [(ICScannerDevice *)device setTransferMode:ICScannerTransferModeMemoryBased];
+        [(ICScannerDevice *)device setDocumentUTI:@"kUTTypePDF"];
+    }
+}
+
+//FIXME: Error when removing device
+- (void)deviceBrowser:(ICDeviceBrowser *)browser didRemoveDevice:(ICDevice *)device moreGoing:(BOOL)moreGoing {
+    //Remove the device from the scanners array
+    [scannersController removeObject:device];
+    [device requestCloseSession];
+}
+
+//FIXME: Error when removing device
+- (void)didRemoveDevice:(ICDevice *)device {
+    //Remove the device from the scanners array
+    [scannersController removeObject:device];
+    [device requestCloseSession];
+}
+
+//MARK: - Scanner Device Delegate Methods
+//TODO: Handle error
+- (void)device:(ICDevice *)device didOpenSessionWithError:(NSError *)error {
+    //Called when a device opens a session
+}
+
+//TODO: Handle error
+- (void)scannerDevice:(ICScannerDevice *)scanner didSelectFunctionalUnit:(ICScannerFunctionalUnit *)functionalUnit error:(NSError *)error {
+    //Called when a device selects a functional unit
+}
+
+//???: Better way to redraw pdf view
+- (void)scannerDevice:(ICScannerDevice *)scanner didScanToURL:(NSURL *)url data:(NSData *)data {
+    //Create a pdf page from the data
+    NSImage *image = [[NSImage alloc] initWithData:data];
+    PDFPage *page = [[PDFPage alloc] initWithImage:image];
+    
+    //If the pdf view has a document
+    if ([pdfView document]) {
+        //Set the page number and add it to the document
+        [page setValue:[NSString stringWithFormat:@"%d", [[pdfView document] pageCount] + 1] forKey:@"label"];
+        [[pdfView document] insertPage:page atIndex:[[pdfView document] pageCount]];
+    } else {
+        //Create a new document and add the page
+        [page setValue:@"1" forKey:@"label"];
+        PDFDocument *document = [[PDFDocument alloc] init];
+        [document insertPage:page atIndex:0];
+        [pdfView setDocument:document];
+    }
+    
+    //Force a redraw for the pdf view so the pages are shown properly
+    [pdfView zoomIn:self];
+    [pdfView setAutoScales:YES];
+}
+
+//TODO: Handle error
+- (void)device:(ICDevice *)device didCloseSessionWithError:(NSError *)error {
+    //Called when a device closes a session
+}
+
+//MARK: - Interface Builder Actions
+- (IBAction)scan:(id)sender {
+    //Get the selected scanner and it's functional unit
+    ICScannerDevice *scanner = [self selectedScanner];
+    ICScannerFunctionalUnit *unit = [scanner selectedFunctionalUnit];
+    
+    //If there is no scan or overviewscan in progress
+    if (![unit overviewScanInProgress] && ![unit scanInProgress]) {
+        //Setup the functional unit and start the scan
+        [unit setScanArea:NSMakeRect(0.0, 0.0, 1240.5, 1753.5)];
+        [unit setResolution:[[unit supportedResolutions] indexGreaterThanOrEqualToIndex:150]];
+        [unit setBitDepth:ICScannerBitDepth8Bits];
+        [unit setPixelDataType:ICScannerPixelDataTypeGray];
+        [scanner requestScan];
+    } else {
+        //Cancel the ongoing scan
+        [scanner cancelScan];
+    }
+}
+
+//MARK: - Class Variables
+- (ICScannerDevice *)selectedScanner {
+    //Return the scanner that is currently selected in the popup
+    return [[scannersController selectedObjects] objectAtIndex:0];
+}
+
+- (BOOL)scannerAvailable {    
+    //If there are no scanners available
+    if ([scanners count] == 0) {
+        return NO;
+    } else {
+        return YES;
+    }    
 }
 
 @end
